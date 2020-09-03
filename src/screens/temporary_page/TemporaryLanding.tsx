f1f4bc4455209d1112904ed8638fe26b018de860
kaimihata <kaimihata@gmail.com>
2020-09-02 21:06:53 -0700

    Update to mystify

--- src/screens/temporary_page/TemporaryLanding.tsx
+++ src/screens/temporary_page/TemporaryLanding.tsx
@@ -4,12 +4,28 @@ import { View, Text, Image } from 'react-native'
 export default function TemporaryLanding() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image source={require('../../assets/images/app_icons/3x/Portals@3x.png')} style={{ width: 300, height: 300 }} />
-      <Text style={{
-        fontFamily: 'OpenSans_300Light',
-        fontSize: 36,
-        paddingVertical: 20,
-      }}>Coming Soon, Portalverse Inc.</Text>
+      <Image
+        source={require('../../assets/images/app_icons/3x/Portals@3x.png')}
+        style={{ width: 300, height: 300 }}
+      />
+      <Text
+        style={{
+          fontFamily: 'OpenSans_300Light',
+          fontSize: 36,
+          paddingVertical: 10,
+        }}
+      >
+        Mystify, Inc.
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'OpenSans_300Light',
+          fontSize: 24,
+          paddingVertical: 5,
+        }}
+      >
+        Coming Soon
+      </Text>
     </View>
-  )
+  );
 }

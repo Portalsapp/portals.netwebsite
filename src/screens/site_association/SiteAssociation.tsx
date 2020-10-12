dca6c541c5a79665ed5f9ef49c85d3a06b3946b5
kaimihata <kaimihata@gmail.com>
2020-10-12 15:09:25 -0700

    Update

--- src/screens/site_association/SiteAssociation.tsx
+++ src/screens/site_association/SiteAssociation.tsx
@@ -3,30 +3,36 @@ import { View, Text } from 'react-native';
 
 export default function SiteAssociation() {
   useEffect(() => {
-    const url = window.URL.createObjectURL(new Blob());
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(
-      new Blob(
-        [
-          `{
-    "applinks": {
-        "apps": [],
-        "details": [
-            {
-                "appID": "X3ULG7P9GJ.net.mystify",
-                "paths": ["*"]
-            }
-        ]
-    }
-}`,
-        ],
-        { type: '/' }
-      )
+    window.open(
+      'https://amplify-portalsmobileapp-dev-150503-deployment.s3-us-west-2.amazonaws.com/public/apple-app-site-association',
+      '_self'
     );
-    link.setAttribute('download', 'apple-app-site-association');
-    // link.download = 'download';
-    document.body.appendChild(link);
-    link.click();
+
+    // const url = window.URL.createObjectURL(new Blob());
+    // const link = document.createElement('a');
+//     link.href = URL.createObjectURL(
+//       new Blob(
+//         [
+//           `{
+//     "applinks": {
+//         "apps": [],
+//         "details": [
+//             {
+//                 "appID": "X3ULG7P9GJ.net.mystify",
+//                 "paths": ["*"]
+//             }
+//         ]
+//     }
+// }`,
+//         ],
+//         { type: '/' }
+//       )
+//     );
+    // link.href = 
+    // link.setAttribute('download', 'apple-app-site-association');
+    // // link.download = 'download';
+    // document.body.appendChild(link);
+    // link.click();
   }, []);
 
   return <View />;

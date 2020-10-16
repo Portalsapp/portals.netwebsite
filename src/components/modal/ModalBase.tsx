d15351a32ccaa9b1d748e54bba29c840b1efe22b
kaimihata <kaimihata@gmail.com>
2020-10-16 00:16:53 -0700

    all static

--- src/components/modal/ModalBase.tsx
+++
@@ -1,6 +0,0 @@
-import { Modal as MobileModal, Platform } from 'react-native'
-/*@ts-ignore*/
-import { Modal as WebModal } from 'modal-react-native-web'
-
-const Modal = Platform.OS === 'web' ? WebModal : MobileModal;
-export default Modal;
--- src/screens/bank/BankScreenStyle.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Platform } from 'react-native'
-import mobileStyle from './BankScreenMobileStyle'
-
-const webStyle = StyleSheet.create({
-
-});
-
-const style = Platform.OS === 'web' ? webStyle : mobileStyle;
-export default style;
